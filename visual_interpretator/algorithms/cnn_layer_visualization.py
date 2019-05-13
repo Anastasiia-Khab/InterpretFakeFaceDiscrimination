e85e35132ad4c3639fde59cf29b6d2d0ec71a007
Vadym Korshunov <vaden4d@gmail.com>
2019-05-14 01:23:27 +0300

    fixed issues

--- visual_interpretator/algorithms/cnn_layer_visualization.py
+++ visual_interpretator/algorithms/cnn_layer_visualization.py
@@ -36,7 +36,7 @@ class CNNLayerVisualization():
         # Hook the selected layer
         self.model[self.selected_layer].register_forward_hook(hook_function)
 
-    def visualise_layer_with_hooks(self, shape, save=False, logs=False, epochs=30):
+    def visualise_layer_with_hooks(self, save=False, logs=False, epochs=30):
         # Hook the selected layer
         self.hook_layer()
         # Generate a random image
