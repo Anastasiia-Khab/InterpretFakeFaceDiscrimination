a5b642e49dea1dd453d54ad0b3d15fd19b727061
anton.shcherbyna <31895978+antonshcherbyna@users.noreply.github.com>
2019-05-13 03:36:23 +0300

    gitignore + licenses + stylegan_generator are added, stargan is renamed, notebook is deleted

---
+++ stylegan_generator/generator.py
@@ -0,0 +1,47 @@
+import torch
+from torchvision.utils import make_grid
+
+from PIL import Image
+
+from model import StyledGenerator
+
+def generate_fakes(num_images):
+    generator = StyledGenerator(512).cuda(2)
+    generator.load_state_dict(torch.load('checkpoint/weights.model)['generator'])
+
+    mean_style = None
+    step = 6
+    shape = 4 * 2 ** step
+
+    for i in range(10):
+        style = generator.mean_style(torch.randn(1024, 512).cuda(2))
+
+        if mean_style is None:
+            mean_style = style
+        else:
+            mean_style += style
+
+    mean_style /= 10
+
+    with torch.no_grad():
+        images = generator(
+            torch.randn(num_images, 512).cuda(2),
+            step=step,
+            alpha=1,
+            mean_style=mean_style,
+            style_weight=0.7,
+        )
+
+    images = postprocess_images(images)
+
+    return images
+
+def postprocess_images(images):
+    processed_images = []
+    for image in images:
+        processed_image = make_grid(image, nrow=1, normalize=True, range=(-1, 1))
+        processed_image = processed_image.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy()
+        processed_image = Image.fromarray(processed_image)
+        processed_images.append(processed_image)
+
+    return processed_images
