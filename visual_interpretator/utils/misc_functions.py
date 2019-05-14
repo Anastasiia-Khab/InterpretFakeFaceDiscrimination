d2f2ddea295114ba3a5ac5aabd3f0ee911d4b847
ylochman <lochman.y@gmail.com>
2019-05-14 22:29:28 +0300

    fixes

--- visual_interpretator/utils/misc_functions.py
+++ visual_interpretator/utils/misc_functions.py
@@ -87,7 +87,7 @@ def apply_colormap_on_image(org_im, activation, colormap_name):
     no_trans_heatmap = color_map(activation)
     # Change alpha channel in colormap to make sure original image is displayed
     heatmap = copy.copy(no_trans_heatmap)
-    heatmap[:, :, 3] = 0.4
+    heatmap[:, :, 3] = activation #0.4
     heatmap = Image.fromarray((heatmap*255).astype(np.uint8))
     no_trans_heatmap = Image.fromarray((no_trans_heatmap*255).astype(np.uint8))
 
